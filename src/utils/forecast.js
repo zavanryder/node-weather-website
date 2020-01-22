595ebab3b7ea4d0369bdfd914e9db31987322d16
root <root@localhost.localdomain>
2020-01-21 16:32:25 -0800

    Added more forecast data: high and low temps

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (lat, lon, callback) => {
             callback('Unable to find location, asshole.', undefined)
         } else {
             callback(undefined, body.daily.data[0].summary + '  Hey asshole, it is currently ' + body.currently.temperature 
-            + ' degrees with ' + body.currently.precipProbability * 100 + '% chance of fuckin rain.')
+            + ' degrees with ' + body.currently.precipProbability * 100 + '% chance of fuckin rain.  The high temperature is ' 
+            + body.daily.data[0].temperatureHigh + '.  The low temperature is ' + body.daily.data[0].temperatureLow)
         }
     })
 }
